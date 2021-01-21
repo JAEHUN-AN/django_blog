a7eb473f5ba3258632d8101f79cc5e33f19425ff
jaehun <dybe15@gmail.com>
2021-01-21 17:31:41 +0900

    blog

---
+++ bookmarkapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView, DetailView
+from bookmarkapp.models import Bookmark
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
+
+# class ApiBookmark(APIView):
+#     def get(self, request):
+#         return Response("ok", status=200)
